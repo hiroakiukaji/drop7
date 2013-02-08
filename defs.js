0c1ac42f39ddbb88b0ffdb6a392d07909ee270d3
Pavel Lishin <pavel.lishin@gmail.com>
2013-02-08 17:24:12 -0500

    Minimum viable game... again

---
+++ defs.js
@@ -0,0 +1,89 @@
+TILE = function(row, col, val) {
+    this.row = row;
+    this.col = col;
+    this.val = val;
+    this.element = $('<div class="tile"></div>').data( { row: row, col: col, val: val, } );
+    this.destroyed = false;
+};
+TILE.prototype = {
+    toString: function() {
+        return "[" + this.val + " @ " + this.row + "," + this.col + "]";
+    },
+    draw: function() {
+        // draw a tile on the board
+        this.element.attr("class", "tile") // remove any other classes
+            .addClass("tile" + this.val)
+            .css("left", this.col * 31)
+            .css("bottom", this.row * 31)
+            .appendTo("#board");
+    },
+    destroyable: function() {
+        // returns true if tile is eligible for destruction
+        var _this = this;
+
+        if (board.getColumnHeight(this.col) == this.val) {
+            // column's height is equal to this tile's value - destroy!
+            return true;
+        }
+
+        // calculating horizontal values is harder, since htey don't collapse in any given direction
+        // to count horizontal neighbors, scan to the right and left until we hit a zero or go off the board
+        var neighbors_horizontal = 1;
+        for(var col = this.col+1; col < COLS; col++) {
+            var matches = jQuery.grep(tiles, function(tile) { return tile.col == col && tile.row == _this.row; } );
+            if (matches.length == 0) { break; } // hit a zero
+            neighbors_horizontal++;
+        }
+        for(var col = this.col-1; col >= 0; col--) {
+            var matches = jQuery.grep(tiles, function(tile) { return tile.col == col && tile.row == _this.row; } );
+            if (matches.length == 0) { break; } // hit a zero
+            neighbors_horizontal++;
+        }
+
+        if (neighbors_horizontal == this.val) {
+            return true;
+        }
+        return false;
+    },
+    collapsible: function() {
+        // returns number of slots this tile can drop down
+        zeros = 0;
+        for(var row = this.row - 1; row >= 0; row--) {
+            if (board.val(row, this.col) == 0) {
+                zeros++;
+            }
+        }
+        return zeros;
+    },
+    destroy: function() {
+        var _this = this;
+        tiles = $.grep(tiles, function(tile) { return tile !== _this; } );
+    },
+};
+
+BOARD = function() { };
+BOARD.prototype = {
+    val: function(row, col) {
+        var t = $.grep(tiles, function(tile) { return tile.row == row && tile.col == col; } );
+        if (t.length == 0) { return 0; } // no tile at this position
+        return t[0].val;
+    },
+    addTile: function(tile) {
+        tiles.push(tile);
+    },
+    getColumnHeight: function(col) {
+        return $.grep(tiles, function(tile) { return tile.col == col; }).length;
+    },
+    draw: function() {
+        $("#board > div.tile").remove();
+        $.each(tiles, function(i, tile) { tile.draw(); });
+    },
+    getDestroyableTiles: function() {
+        return $.grep(tiles, function(tile) { return tile.destroyable(); } );
+    },
+    getCollapsibleTiles: function() {
+        // returns array of tiles that can be dropped down
+        return $.grep(tiles, function(tile) { return tile.collapsible(); } );
+    },
+};
+
--- drop7.js
+++ drop7.js
@@ -1,24 +1,181 @@
 /*
 TODO:
+ - prevent player from clicking while a drop is happening
 
+
+
+
+LOOP:
+// drop tile
+// loop:
+//   destroy tiles
+//   collapse tiles
+// increment turn
+// check for endgame
+// create droptile
 
 
 */
 
 ROWS = 7;
 COLS = 7;
+VALUES = 7;
 
-LEVEL = 1; // current level
-TURNS = 5; // turns left on this level
-SCORE = 0; // user's current score
+ANIM_DROPTILE_HORIZONTAL = 33;
+ANIM_DROPTILE_VERTICAL = 33;
+ANIM_DESTROY = 500;
 
-BOARD = [ [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0] ];
+level = 1; // current level
+turns = 5; // turns left on this level
+score = 0; // user's current score
+
+tiles = [];
+board = new BOARD();
+droptile = null;
+
+function start_game() {
+    // Populate the board with random tiles
+    var tile_count = Math.floor(Math.random() * 25);
+
+    for(var i=0; i<tile_count; i++) {
+        var col = Math.floor(Math.random() * COLS);
+        for(var row = 0; row < ROWS && board.val(row, col) != 0; row++);
+        if (row == ROWS) { i--; tile_count--; continue; } // this column is full
+        var val = Math.ceil(Math.random() * VALUES);
+        board.addTile( new TILE(row, col, val) );
+    }
+
+    // destroy and collapse the tiles as necessary, if any are collapsible
+    do {
+        var destroyable = board.getDestroyableTiles();
+        $.each(destroyable, function(i, tile) { tile.destroy(); } );
+        var collapsible = board.getCollapsibleTiles();
+        $.each(collapsible, function(i, tile) { tile.row -= tile.collapsible(); } );
+    } while (destroyable.length > 0 || collapsible.length > 0);
+
+    // draw the board
+    board.draw();
+
+    // create a tile that's ready to be dropped.
+    create_droptile();
+}
+
+function create_droptile() {
+    // create a tile that's ready to be dropped.
+    droptile = new TILE(ROWS, Math.floor(COLS / 2), Math.ceil(Math.random() * VALUES));
+    droptile.draw();
+
+    // listen for user dropping
+    allow_clicks();
+}
+
+function allow_clicks() {
+    $('div.cell').on('click', drop);
+}
+function forbid_clicks() {
+    $('div.cell').off('click');
+}
+
+function drop() {
+    forbid_clicks();
+
+    var col = $(this).data('col');
+    var row = board.getColumnHeight(col);
+
+    console.log("Player clicked on column", $(this).data('col'), 'will be shoving into', row);
+
+    // is column full?
+    if (row > ROWS) { allow_clicks(); return; }
+
+    // Place droptile onto the board
+    board.addTile(droptile);
+
+    // animate droptile
+
+    droptile.element
+        .animate( { left: col * 31, }, ANIM_DROPTILE_HORIZONTAL * Math.abs(droptile.col - col))
+        .animate( { bottom: row * 31, }, ANIM_DROPTILE_VERTICAL * Math.abs(droptile.row - row))
+        .promise().done( function() {
+            console.log("Done animating");
+            droptile.row = row;
+            droptile.col = col;
+
+            destroy_tiles();
+        });
+}
+
+function destroy_tiles(chain) {
+    if (typeof(chain) == undefined) { chain = 1; }
+
+    // destroy tiles
+    var destroyable = board.getDestroyableTiles();
+    var wait = $.Deferred();
+    var destroyed = destroyable.length;
+
+    console.log("There are", destroyable.length, "tiles waiting to be destroyed");
+
+    wait.progress( function(notification) {
+        console.log("Received notification! ", notification, destroyed);
+        destroyed++;
+        if (destroyed >= destroyable.length) {
+            wait.resolve();
+        }
+    }).done( function() {
+        // if there are any collapsible tiles, call collapse_tiles
+        console.log("We are done destroying tiles");
+        if (board.getCollapsibleTiles.length > 0) {
+            collapse_tiles(chain+1);
+        } else {
+            increment_turn();
+        }
+    } );
+
+    $.each(destroyable, function(i, tile) {
+        // animate this tile's destruction
+        tile.element
+            .animate( { width: '+=10', height: '+=10', left: '-=5', bottom: '-=5', }, ANIM_DESTROY )
+            .animate( { width: '-=10', height: '-=10', left: '+=5', bottom: '+=5', opacity: 0, }, ANIM_DESTROY )
+            .promise()
+            .done( function() {
+                tile.destroy();
+
+                // notify the Deferred object wait that we are done with a tile
+                wait.notify("Hey, tile" + tile + " is done being destroyed now");
+
+            } )
+
+    });
+
+
+}
+
+function collapse_tiles(chain) {
+    if (typeof(chain) == undefined) { chain = 1; }
+
+    // collapse tiles
+
+    // if there are any destroyable tiles, call destroy_tiles
+    if (board.getDestroyableTiles.length > 0) {
+        destroy_tiles(chain+1);
+    } else {
+        increment_turn();
+    }
+}
+
+function increment_turn() {
+    // TODO
+
+    // increment turn
+    // if end of level reached, increase level
+    // check for endgame condition
+    // create droptile
+    create_droptile();
+}
+
+
+
+
+
 
 
 $(document).ready( function() {
@@ -29,8 +186,5 @@ $(document).ready( function() {
         }
     }
 
-    // attach listeners
-    $("div.cell").on('click', function() {
-        console.log("Player clicked on column", $(this).data('col'));
-    });
+    start_game();
 });
